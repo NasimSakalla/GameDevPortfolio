40f826113aaefa5cb6f97426a8a8e269a2e4607d
nipnzo <niponiso019@gmail.com>
2025-02-26 03:45:58 +0200

    Card-Memory-Game

---
+++ Assets/Tests/Edit Mode Tests/TestingRayDepth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestingRayDepth
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void TestingRayDepthSanityTest()
+    {
+        //TO MAKE THE TEST WORK CHANGE THE FUNCTION AND THE RAYDEPTH INTO PUBLIC VAR'S
+
+        //GameObject temp = GameObject.Find("Game Manager");
+        //temp.GetComponent<RaycastEngine>().CreatingTheRaycast();
+        //int raycastDepth = temp.GetComponent<RaycastEngine>().rayDepth;
+        //Assert.AreEqual(10,raycastDepth);
+    }
+}
